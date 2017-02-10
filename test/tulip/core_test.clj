e51e9f52f55d0faa2eb84be9d309710fda1f174a
Monade <iomonad@users.noreply.github.com>
2017-02-10 22:54:47 +0100

    Added routes tests

--- test/tulip/core_test.clj
+++ test/tulip/core_test.clj
@@ -5,3 +5,12 @@
 (deftest a-test
   (testing "Trivial test to accept travis build"
     (is (= 1 1))))
+
+(deftest test-app
+  (testing "Testing users POST ROUTE"
+    (let [response (app (request :post "/users"))]
+      (is (= (:status response) 200))
+      (is (= (get-in response [:headers "Content-Type"]) "application-json"))))
+  (testing "Testing 404 pages."
+    (let [response (app (request :get "/bogus-route"))]
+      (is (= (:status response) 404)))))
